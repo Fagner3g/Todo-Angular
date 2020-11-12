b3b8dff8619e390aed13bc6c14e07cda2e41e54c
Fagner Gomes <fagner@MacBook-Air-de-Fagner.local>
2020-11-12 19:22:15 -0300

    feat: 🎸 Adicionamos um componente manualmente
    Adicionar componente manualmente ao projeto

---
+++ src/components/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>meu primeiro componete com angular 2 hahahaha</p>
+  `
+})
+
+export class MeuPrimeiroComponent {
+
+}
